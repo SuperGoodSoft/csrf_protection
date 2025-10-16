0d68c06bcb93a1a7440d96f9edba76fe4b8adb46
Sofia Besenski <sofiabesenski4@gmail.com>
2025-10-16 10:50:18 -0700

    Initial commit
    Co-authored-by: Jared Norman <jared@super.gd>
    Co-authored-by: Senem Soy <senem@super.gd>
    Co-authored-by: Alistair Norman <alistair@super.gd>

---
+++ lib/rack/sec_fetch_site.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sec_fetch_site/version"
+
+module Rack
+  module SecFetchSite
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rack/sec_fetch_site_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rack::SecFetchSite do
+  it "has a version number" do
+    expect(Rack::SecFetchSite::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
