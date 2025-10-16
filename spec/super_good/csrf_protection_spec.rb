6f024fb19d390247c15b3ad8ac5dd44a4333dbb9
Jared Norman <jared@super.gd>
2025-10-16 16:38:37 -0700

    Handle other HTTP methods

--- spec/super_good/csrf_protection_spec.rb
+++ spec/super_good/csrf_protection_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe SuperGood::CSRFProtection do
   subject { described_class.new(app) }
   let(:app) { proc { |env| [200, {"Content-Type" => "text/plain"}, ["OK"]] } }
   let(:get_env) { Rack::MockRequest.env_for("http://example.com:8080/", {:method => "GET", "HTTP_SEC_FETCH_SITE" => sec_fetch_site}) }
+  let(:head_env) { Rack::MockRequest.env_for("http://example.com:8080/", {:method => "HEAD", "HTTP_SEC_FETCH_SITE" => sec_fetch_site}) }
+  let(:options_env) { Rack::MockRequest.env_for("http://example.com:8080/", {:method => "OPTIONS", "HTTP_SEC_FETCH_SITE" => sec_fetch_site}) }
   let(:post_env) { Rack::MockRequest.env_for("http://example.com:8080/", {:method => "POST", "HTTP_SEC_FETCH_SITE" => sec_fetch_site}) }
+  let(:put_env) { Rack::MockRequest.env_for("http://example.com:8080/", {:method => "PUT", "HTTP_SEC_FETCH_SITE" => sec_fetch_site}) }
+  let(:delete_env) { Rack::MockRequest.env_for("http://example.com:8080/", {:method => "DELETE", "HTTP_SEC_FETCH_SITE" => sec_fetch_site}) }
 
   context "when the Sec-Fetch-Site contains cross-site" do
     let(:sec_fetch_site) { "cross-site" }
@@ -13,8 +17,24 @@ RSpec.describe SuperGood::CSRFProtection do
       expect { subject.call(post_env) }.to raise_error(SuperGood::CSRFProtection::Error)
     end
 
+    it "raises an error for PUT" do
+      expect { subject.call(put_env) }.to raise_error(SuperGood::CSRFProtection::Error)
+    end
+
+    it "raises an error for DELETE" do
+      expect { subject.call(delete_env) }.to raise_error(SuperGood::CSRFProtection::Error)
+    end
+
     it "calls the app for GET" do
       expect(subject.call(get_env).first).to eq(200)
+    end
+
+    it "calls the app for HEAD" do
+      expect(subject.call(head_env).first).to eq(200)
+    end
+
+    it "calls the app for OPTIONS" do
+      expect(subject.call(options_env).first).to eq(200)
     end
   end
 
@@ -25,8 +45,24 @@ RSpec.describe SuperGood::CSRFProtection do
       expect(subject.call(post_env).first).to eq(200)
     end
 
+    it "calls the app for PUT" do
+      expect(subject.call(put_env).first).to eq(200)
+    end
+
+    it "calls the app for DELETE" do
+      expect(subject.call(delete_env).first).to eq(200)
+    end
+
     it "calls the app for GET" do
       expect(subject.call(get_env).first).to eq(200)
+    end
+
+    it "calls the app for HEAD" do
+      expect(subject.call(head_env).first).to eq(200)
+    end
+
+    it "calls the app for OPTIONS" do
+      expect(subject.call(options_env).first).to eq(200)
     end
   end
 end
