2f668f9d0f1ca2216d3c821a1cfa2c8f55175e4b
Jared Norman <jared@super.gd>
2025-10-17 17:34:14 -0700

    Fall back to using Origin/Host headers

--- spec/super_good/csrf_protection_spec.rb
+++ spec/super_good/csrf_protection_spec.rb
@@ -9,39 +9,61 @@ RSpec.describe SuperGood::CSRFProtection do
   let(:env) {
     Rack::MockRequest.env_for("http://example.com:8080/", {
       :method => method,
-      "HTTP_SEC_FETCH_SITE" => sec_fetch_site
+      "HTTP_SEC_FETCH_SITE" => sec_fetch_site,
+      "HTTP_ORIGIN" => origin,
+      "HTTP_HOST" => host
     })
   }
+  let(:sec_fetch_site) { nil }
+  let(:origin) { nil }
+  let(:host) { nil }
 
-  shared_examples "allows request" do |method|
-    context method do
-      let(:method) { method }
-      let(:raise_error_option) { true }
+  shared_examples "allows safe HTTP methods" do
+    %w[GET HEAD OPTIONS].each do |http_method|
+      context http_method do
+        let(:method) { http_method }
+        let(:raise_error_option) { true }
 
-      it "allows the request" do
-        expect(subject.first).to eq(200)
+        it "allows the request" do
+          expect(subject.first).to eq(200)
+        end
       end
     end
   end
 
-  shared_examples "denies request" do |method|
-    context method do
-      let(:method) { method }
-
-      context "when raise_error is true" do
+  shared_examples "allows unsafe HTTP methods" do
+    %w[POST PUT PATCH DELETE].each do |http_method|
+      context http_method do
+        let(:method) { http_method }
         let(:raise_error_option) { true }
 
-        it "raises an error" do
-          expect { subject }.to raise_error(SuperGood::CSRFProtection::Error)
+        it "allows the request" do
+          expect(subject.first).to eq(200)
         end
       end
+    end
+  end
 
-      context "when raise_error is false" do
-        let(:raise_error_option) { false }
+  shared_examples "denies unsafe HTTP methods" do
+    %w[POST PUT PATCH DELETE].each do |http_method|
+      context http_method do
+        let(:method) { http_method }
 
-        it "returns a 403 response" do
-          expect(subject)
-            .to eq([403, {"Content-Type" => "text/plain"}, ["Forbidden"]])
+        context "when raise_error is true" do
+          let(:raise_error_option) { true }
+
+          it "raises an error" do
+            expect { subject }.to raise_error(SuperGood::CSRFProtection::Error)
+          end
+        end
+
+        context "when raise_error is false" do
+          let(:raise_error_option) { false }
+
+          it "returns a 403 response" do
+            expect(subject)
+              .to eq([403, {"Content-Type" => "text/plain"}, ["Forbidden"]])
+          end
         end
       end
     end
@@ -50,48 +72,96 @@ RSpec.describe SuperGood::CSRFProtection do
   context "when the Sec-Fetch-Site contains cross-site" do
     let(:sec_fetch_site) { "cross-site" }
 
-    include_examples "allows request", "GET"
-    include_examples "allows request", "HEAD"
-    include_examples "allows request", "OPTIONS"
-    include_examples "denies request", "POST"
-    include_examples "denies request", "PUT"
-    include_examples "denies request", "DELETE"
+    include_examples "allows safe HTTP methods"
+    include_examples "denies unsafe HTTP methods"
   end
 
   context "when the Sec-Fetch-Site header contains same-origin" do
     let(:sec_fetch_site) { "same-origin" }
 
-    include_examples "allows request", "GET"
-    include_examples "allows request", "HEAD"
-    include_examples "allows request", "OPTIONS"
-    include_examples "allows request", "POST"
-    include_examples "allows request", "PUT"
-    include_examples "allows request", "DELETE"
+    include_examples "allows safe HTTP methods"
+    include_examples "allows unsafe HTTP methods"
   end
 
-  context "when raise_error is false" do
-    let(:raise_error_option) { false }
+  context "when the Sec-Fetch-Site header is missing" do
+    let(:sec_fetch_site) { nil }
 
-    context "when the Sec-Fetch-Site contains cross-site" do
-      let(:sec_fetch_site) { "cross-site" }
+    context "when the Origin header is present" do
+      let(:origin) { "http://example.com:8080" }
 
-      include_examples "allows request", "GET"
-      include_examples "allows request", "HEAD"
-      include_examples "allows request", "OPTIONS"
-      include_examples "denies request", "POST"
-      include_examples "denies request", "PUT"
-      include_examples "denies request", "DELETE"
+      context "when it matches the Host header" do
+        let(:host) { "example.com:8080" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "allows unsafe HTTP methods"
+      end
+
+      context "when it doesn't match the Host header" do
+        let(:host) { "differentdomain.com:8080" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
+
+      context "when the Host header isn't present" do
+        let(:host) { nil }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
+
+      context "when the Origin has no port" do
+        let(:origin) { "http://example.com" }
+        let(:host) { "example.com" }
+
+        include_examples "allows unsafe HTTP methods"
+      end
+
+      context "when Origin differs only by port" do
+        let(:origin) { "http://example.com:8080" }
+        let(:host) { "example.com:9000" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
+
+      context "when the Origin differs only by domain" do
+        let(:host) { "sub.example.com:8080" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
+
+      context 'when the Origin header is "null"' do
+        let(:origin) { "null" }
+        let(:host) { "example.com:8080" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
+
+      context "when the Origin header is malformed" do
+        let(:origin) { "http://////:////" }
+        let(:host) { "example.com:8080" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
+
+      context "when the Host header is malformed" do
+        let(:host) { "::::::" }
+
+        include_examples "allows safe HTTP methods"
+        include_examples "denies unsafe HTTP methods"
+      end
     end
 
-    context "when the Sec-Fetch-Site header contains same-origin" do
-      let(:sec_fetch_site) { "same-origin" }
+    context "when the Origin header is missing" do
+      let(:origin) { nil }
+      let(:host) { "example.com:8080" }
 
-      include_examples "allows request", "GET"
-      include_examples "allows request", "HEAD"
-      include_examples "allows request", "OPTIONS"
-      include_examples "allows request", "POST"
-      include_examples "allows request", "PUT"
-      include_examples "allows request", "DELETE"
+      include_examples "allows safe HTTP methods"
+      include_examples "allows unsafe HTTP methods"
     end
   end
 end
